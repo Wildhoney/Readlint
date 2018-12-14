314af4613caab1068054bb8f94b79abf341a4ced
Wildhoney <adam.timberlake@gmail.com>
2018-12-14 13:05:23 +0000

    Renamed mock MD files

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -3,16 +3,16 @@ import test from 'ava';
 import runTest from '../';
 
 test('It should be able to parse JS examples with full URL imports;', async t => {
-    const file = path.resolve('./src/__tests__/mocks/first.md');
+    const file = path.resolve('./src/__tests__/mocks/URL_IMPORT.md');
     t.deepEqual(await runTest(file), []);
 });
 
 test('It should be able to parse JS examples with node style requires;', async t => {
-    const file = path.resolve('./src/__tests__/mocks/second.md');
+    const file = path.resolve('./src/__tests__/mocks/NODE_REQUIRE.md');
     t.deepEqual(await runTest(file), []);
 });
 
 test('It should be able to parse CSS examples;', async t => {
-    const file = path.resolve('./src/__tests__/mocks/third.md');
+    const file = path.resolve('./src/__tests__/mocks/BASIC_CSS.md');
     t.deepEqual(await runTest(file), []);
 });
