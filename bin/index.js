995fe9bf4801f05f8b55214e81a2cff2650405d8
Wildhoney <adam.timberlake@gmail.com>
2018-12-14 18:22:31 +0000

    Reverted to Lintme

--- bin/index.js
+++ bin/index.js
@@ -14,7 +14,7 @@ async function main() {
     const report = await runTest(filename);
     const isValid = R.isEmpty(report);
 
-    figlet('Lintmd', { font: 'univers' }, (_, data) => {
+    figlet('Lintme', { font: 'univers' }, (_, data) => {
         console.log(chalk.gray(data));
 
         report.forEach(({ type, line, column, message }) => {
