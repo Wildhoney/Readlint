791694bfa7a035501c97da6798939dcb2b383cda
Wildhoney <adam.timberlake@gmail.com>
2018-12-15 18:50:34 +0000

    Added version to the node output

--- bin/index.js
+++ bin/index.js
@@ -2,20 +2,26 @@
 
 require('@babel/register');
 
+const fs = require('fs');
+const path = require('path');
 const findUp = require('find-up');
 const chalk = require('chalk');
 const figlet = require('figlet');
 const R = require('ramda');
 const message = require('msg-generator');
+const capitalise = require('capitalize');
 const runTest = require('../src/index.js').default;
+
+const pkg = JSON.parse(fs.readFileSync(path.resolve('./package.json'), 'utf8'));
 
 async function main() {
     const filename = await findUp('README.md');
     const report = await runTest(filename);
     const isValid = R.isEmpty(report);
 
-    figlet('Lintread', { font: 'univers' }, (_, data) => {
+    figlet(capitalise(pkg.name), { font: 'univers' }, (_, data) => {
         console.log(chalk.gray(data));
+        console.log(chalk.gray('Version:'.padStart(70)), pkg.version, '\n\n');
 
         report.forEach(({ type, line, column, message }) => {
             console.log(
